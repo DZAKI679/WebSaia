03e0dc204b99485d688406ab1922eecf676ccffc
DZAKI679 <zakymuklisin@gmail.com>
2026-07-30 17:43:44 +0700

    adding button that can be used to moving the cube

--- gameke-1/game.js
+++ gameke-1/game.js
@@ -1,6 +1,10 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
 
 let player = {
     x: 200,
@@ -9,20 +13,53 @@ let player = {
     speed: 5
 };
 
-
 let keys = {};
 
-
-document.addEventListener("keydown", e => {
+// ===========================
+// Keyboard
+// ===========================
+document.addEventListener("keydown", (e) => {
     keys[e.key] = true;
 });
 
-
-document.addEventListener("keyup", e => {
+document.addEventListener("keyup", (e) => {
     keys[e.key] = false;
 });
 
+// ===========================
+// Button Control
+// ===========================
+function controlButton(button, key) {
 
+    function start(e) {
+        e.preventDefault();
+        keys[key] = true;
+    }
+
+    function stop(e) {
+        e.preventDefault();
+        keys[key] = false;
+    }
+
+    // PC
+    button.addEventListener("mousedown", start);
+    button.addEventListener("mouseup", stop);
+    button.addEventListener("mouseleave", stop);
+
+    // HP
+    button.addEventListener("touchstart", start, { passive: false });
+    button.addEventListener("touchend", stop, { passive: false });
+    button.addEventListener("touchcancel", stop, { passive: false });
+}
+
+controlButton(up, "ArrowUp");
+controlButton(down, "ArrowDown");
+controlButton(left, "ArrowLeft");
+controlButton(right, "ArrowRight");
+
+// ===========================
+// Update
+// ===========================
 function update() {
 
     if (keys["ArrowRight"]) {
@@ -66,7 +103,9 @@ function update() {
     requestAnimationFrame(update);
 }
 
-
+// ===========================
+// Draw
+// ===========================
 function draw() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -80,6 +119,5 @@ function draw() {
         player.size
     );
 }
-
 
 update();
